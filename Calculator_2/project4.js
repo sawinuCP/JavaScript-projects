aee759093c2173d1d14843967d1acef1431c2820
Sawinu Chandira Peiris <savinu.chandira2002@gmail.com>
2023-04-22 09:42:34 +0530

    task

---
+++ Calculator_2/project4.js
@@ -0,0 +1,30 @@
+const buttonsEl = document.querySelectorAll("button");
+
+console.log(buttonsEl)
+
+for(var i=0 ; i < buttonsEl.length ; i ++){
+    buttonsEl[i]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
