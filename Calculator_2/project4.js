4647942258ea03a567b745b6781f4de873881e4c
Sawinu Chandira Peiris <savinu.chandira2002@gmail.com>
2023-04-22 15:17:40 +0530

    finish cal

--- Calculator_2/project4.js
+++ Calculator_2/project4.js
@@ -1,30 +1,33 @@
 const buttonsEl = document.querySelectorAll("button");
+const inputFieldEl = document.getElementById("result");
 
-console.log(buttonsEl)
+// console.log(buttonsEl)
 
-for(var i=0 ; i < buttonsEl.length ; i ++){
-    buttonsEl[i]
+for(let i = 0 ; i < buttonsEl.length ; i ++){
+    buttonsEl[i].addEventListener("click" , () => {
+        const buttonValue = buttonsEl[i].textContent;
+        if(buttonValue === "C" ){
+            clearResult()
+        }
+        else if(buttonValue === "="){
+            calculateResult()
+        }
+        else{
+            appendValue(buttonValue)
+        }
+    });
 }
 
 
+function clearResult(){
+    inputFieldEl.value = "";
+}
 
+function calculateResult(){
+    inputFieldEl.value = eval(inputFieldEl.value)
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function appendValue(buttonValue){
+    inputFieldEl.value += buttonValue;
+    // inputFieldEl.value = buttonValue +buttonValue;
+}
